188c32d23ef3c67cd22f25906d7652e1fcb1eab9
Narek Khachatryan <narek1993x@mail.ru>
2018-12-28 18:32:49 +0400

    install client side of app and bcrypt password in server side

--- server.js
+++ server.js
@@ -3,23 +3,29 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
 
+// Import typeDefs and resolvers
 const filePath = path.join(__dirname, 'typeDefs.gql');
 const typeDefs = fs.readFileSync(filePath, 'utf-8');
 const resolvers = require('./resolvers');
 
+// Import Environment Variables and mongoose Models
 require('dotenv').config({ path: 'variables.env' });
 const User = require('./models/User');
 const Post = require('./models/Post');
 
-// const MONGO_URI = 'mongodb://localhost:27017/fullstack-vue-graphql';
+// Connect to MLab Database
+
+// Local URI
+const MONGO_URI = 'mongodb://localhost:27017/fullstack-vue-graphql';
 mongoose
   .connect(
-    process.env.MONGO_URI,
+    MONGO_URI,
     { useNewUrlParser: true, useCreateIndex: true }
   )
   .then(() => console.log('Connected to MongoLab instance.'))
   .catch((error) => console.log('Error connecting to MongoLab:', error));
 
+// Create Apollo/GraphQL Server using typeDefs, resolvers and context object
 const server = new ApolloServer({
   typeDefs,
   resolvers,
